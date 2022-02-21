d8fd1131fea1b05f6614bb32d4eb31e45fa21099
Berkan Acar <berkanacar@gmail.com>
2022-02-21 23:03:52 +0100

    find employee functionality test with static initial list controls

--- src/test/java/com/bleucrm/pages/FindEmployeePage.java
+++ src/test/java/com/bleucrm/pages/FindEmployeePage.java
@@ -5,20 +5,49 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
 
-
+import java.util.ArrayList;
 import java.util.List;
 
 public class FindEmployeePage extends BasePage {
 
 
- @FindBy (id="pagetitle")
- public WebElement FindEmployeeTitle;
+  @FindBy(id = "pagetitle")
+  public WebElement FindEmployeeTitle;
 
- @FindAll({
-         @FindBy(xpath = "//*[.='Search By Alphabet']"),
-         @FindBy(className = "filter-but-text")
- })
- public WebElement SearchByAlphabetButton;
+  @FindAll({
+          @FindBy(xpath = "//*[.='Search By Alphabet']"),
+          @FindBy(className = "filter-but-text")
+  })
+  public WebElement SearchByAlphabetButton;
+
+  @FindBy(className = "employee-name-link")
+  public List<WebElement> employees;
+
+  public List<List<String>> initials() {
+
+    ArrayList charsWithSurname = new ArrayList();
+    ArrayList charsWithNameOnly = new ArrayList();
+    String surnameInitial;
+    String nameInitial;
+    for (WebElement employee : employees) {
+      nameInitial= String.valueOf(employee.getText().split(" ")[0].charAt(0)).toUpperCase();
+
+      if (employee.getText().split(" ").length == 2) {
+        surnameInitial= String.valueOf(employee.getText().split(" ")[1].charAt(0)).toUpperCase();
+        if (!charsWithSurname.contains(surnameInitial)){
+          charsWithSurname.add(surnameInitial);
+        }
+      }else if ((!charsWithNameOnly.contains(nameInitial))&&(!charsWithSurname.contains(nameInitial))) {
+        charsWithNameOnly.add(nameInitial);
+        }
+      }
+
+    List<List<String>> initialsList = new ArrayList<List<String>>();
+    initialsList.add(charsWithSurname);
+    initialsList.add(charsWithNameOnly);
+
+    return initialsList;
+  }
 
 
 }
--- src/test/java/com/bleucrm/step_definitions/FindEmployeeSteDefs.java
+++ src/test/java/com/bleucrm/step_definitions/FindEmployeeSteDefs.java
@@ -3,37 +3,44 @@ package com.bleucrm.step_definitions;
 import com.bleucrm.pages.EmployeesPage;
 import com.bleucrm.pages.FindEmployeePage;
 import com.bleucrm.utilities.Driver;
-import io.cucumber.java.an.E;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+
 import static com.bleucrm.utilities.BrowserUtils.*;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.Locale;
 
 
 public class FindEmployeeSteDefs {
+
+  public static List<List<String>> initials;
+
   @When("the user click on Find Employee menu")
-  public void the_user_click_on_Find_Employee_menu() {
-    EmployeesPage employeesPage=new EmployeesPage();
-    waitForClickability(employeesPage.findEmployeeButton,15);
+  public static void the_user_click_on_Find_Employee_menu() {
+    EmployeesPage employeesPage = new EmployeesPage();
+    waitForClickability(employeesPage.findEmployeeButton, 15);
     employeesPage.findEmployeeButton.click();
 
   }
 
-  @Then("{string} title is displayed")
-  public void titleIsDisplayed(String expectedtitle) {
+  @Then("{string} title is displayed with the user list")
+  public void titleIsDisplayedWithTheUserList(String expectedtitle) {
     FindEmployeePage findEmployeePage = new FindEmployeePage();
+    initials= findEmployeePage.initials();
     String actualTitle = findEmployeePage.FindEmployeeTitle.getText();
     Assert.assertEquals("Actual title is different tha expected", expectedtitle, actualTitle);
+
+    Assert.assertFalse(initials.isEmpty());
+    System.out.println("[Surname initials, Only Name initials]\n" + initials.toString());
+
   }
 
-  @And("the user  clicks Search By Alphabet button")
+
+  @When("the user  clicks Search By Alphabet button")
   public void the_user_clicks_Search_By_Alphabet_button() {
     new FindEmployeePage().SearchByAlphabetButton.click();
 
@@ -42,18 +49,29 @@ public class FindEmployeeSteDefs {
   @And("the user clicks the initial character {string} of the searched user")
   public void the_user_clicks_the_initial_character_of_the_searched_user(String character) {
     Driver.get().findElement(By.linkText(character)).click();
-    waitForVisibility(Driver.get().findElement(By.xpath("//*[.='Search By Alphabet: "+character+"' and @class='filter-but-text']")),5);
+    waitForVisibility(Driver.get().findElement(By.xpath("//*[.='Search By Alphabet: " + character + "' and @class='filter-but-text']")), 10);
+
   }
 
   @Then("the information of the user with initial {string} is displayed")
-  public void the_information_of_the_user_with_initial_is_displayed(String expectedCharacter) {
-    List<WebElement> resulting_employees = Driver.get().findElements(By.className("employee-name-link"));
-    Assert.assertFalse(resulting_employees.isEmpty());
-    for (WebElement resulting_employee : resulting_employees) {
-      String actualCharacter=String.valueOf(resulting_employee.getText().split(" ")[1].charAt(0)).toUpperCase();;
-      Assert.assertEquals("actual differs from expected",expectedCharacter,actualCharacter);
+  public void the_information_of_the_user_with_initial_is_displayed(String expectedCharacter)  {
+
+        List<WebElement> resulting_employees = Driver.get().findElements(By.className("employee-name-link"));
+    if (initials.get(0).contains(expectedCharacter)) {
+
+      for (WebElement resulting_employee : resulting_employees) {
+        String actualCharacter = String.valueOf(resulting_employee.getText().split(" ")[1].charAt(0)).toUpperCase();
+
+        Assert.assertEquals("actual differs from expected", expectedCharacter, actualCharacter);
+      }
+    } else if (initials.get(1).contains(expectedCharacter)) {
+      Assert.assertFalse("No results for an employee without surname. There are users with "+initials.get(1).toString()+" initials.",resulting_employees.isEmpty());
+    }else{
+
+      Assert.fail("Initial character must be one of the following: \n"+initials.toString());
 
     }
+
 
   }
 
